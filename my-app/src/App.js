ded3d20134274b762231083c28b1789c7eb2be79
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-07 20:07:32 +0100

    mvp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Pocket from './pocket';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Pocket />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
