05e3f72cdddc254af731c0e79eed70223ecf2db1
Juliana Hernandez <juliviolin@gmail.com>
2020-10-07 17:37:50 -0300

    App created using create react app

---
+++ outreachy-first-contribution/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./pocket-logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
