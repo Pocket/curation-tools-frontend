6828211ed59e84997d4141fa800ed1c11a624628
Annette Sunday <annettesunday@Annettes-MacBook-Pro.local>
2020-10-08 01:19:00 +0300

    chore: restructure project directories

---
+++ pocket-ui/src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Initial Screener exercise
+    </div>
+  );
+}
+
+export default App;
