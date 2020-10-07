b1ddb3c860a63d7842c69305304661728feec6fd
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-10-07 18:59:49 +0100

    import button from material ui

--- my-app/src/pocket.js
+++ my-app/src/pocket.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Button from '@material-ui/core/Button';
 
 const Pocket = () => {
     return(
-        <>
+        <div>
             <Avatar alt="Pocket" src="./pocket.jpg" className={classes.large} />
-        </>
+        </div>
     )
 }
 
