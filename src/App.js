85705a2d1ca9b2252c0caf132d95d1371bc22e6d
Lorela Jano <janolorela@yahoo.com>
2020-10-08 01:05:32 +0200

    Updated App.js to show pocket component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Pocket from './Pocket.js';
+
+function App() {
+  return (
+    <div>
+      <Pocket />
+    </div>
+  );
+}
+
+export default App;
