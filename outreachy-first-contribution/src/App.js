01ae9f8a633dc005123ce7d7d10cb2b321b42b98
Juliana Hernandez <juliviolin@gmail.com>
2020-10-07 18:19:02 -0300

    Create the component using Material UI

--- outreachy-first-contribution/src/App.js
+++ outreachy-first-contribution/src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import logo from "./pocket-logo.svg";
+import { makeStyles } from "@material-ui/core/styles";
+import { Box } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Box
+      className={classes.root}
+      display="flex"
+      flexDirection="row"
+      justifyContent="center"
+    >
+      <Card>
+        <CardMedia className={classes.media} image={logo} title="logo" />
+        <Button variant="outlined" size="small" color="primary">
+          Juliana Hernandez
+        </Button>
+      </Card>
+    </Box>
   );
 }
 
