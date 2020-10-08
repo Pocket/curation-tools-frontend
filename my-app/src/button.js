1d22e0b22d66b62bbcccabc4d04ae76caa4de553
Rohacode <roha.afzal@gmail.com>
2020-10-08 17:13:31 +0500

    Logo and Button Added

---
+++ my-app/src/button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Button from "@material-ui/core/Button";
+
+export default () => {
+  return (
+    <div>
+      <Button size="large" variant="contained" color="secondary">
+        Roha Afzal
+      </Button>
+    </div>
+  );
+};
