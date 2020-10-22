dbaf271427b133889b52bac4a2712a8ce7b53c7c
Joaquin-M2 <joaquinmorenomolina@gmail.com>
2020-10-22 21:03:43 +0200

    Include card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import PocketCard from './components/card/card';
 
 function App() {
-  return (
-    <div className="App">
-      <!-- your component goes here to test the rendering! -->
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <!-- your component goes here to test the rendering! --> */}
+            <PocketCard />
+        </div>
+    );
 }
 
 export default App;
