6ac2571be77f683890577cd0c027bc8ad70f9001
Lali <lauradiaz1586@gmail.com>
2020-10-23 01:34:21 +0400

    create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <!-- your component goes here to test the rendering! -->
+      <Header />
     </div>
   );
 }
