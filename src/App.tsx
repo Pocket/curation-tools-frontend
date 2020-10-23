9f203cebeec7b3c1ce840b10c4de3c505f23526b
Joaquin-M2 <joaquinmorenomolina@gmail.com>
2020-10-23 20:35:23 +0200

    Include Card component with fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import PocketCard from './components/Ccard/Ccard';
+import PocketCard from './components/Card/Card';
 
 function App() {
     return (
