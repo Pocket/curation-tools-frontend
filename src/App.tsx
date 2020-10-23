ed974e47d3bce96882c8172f51d68bd72a335f41
mhtien <59407030+mhtien@users.noreply.github.com>
2020-10-23 23:35:20 +0200

    Create Add Page Story Component
    Use Material UI and ThemeProvider for majority of components and associated styling, create new CSS file for styling h1 html tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddStory from "./components/AddStory"
 
 function App() {
   return (
     <div className="App">
-      <!-- your component goes here to test the rendering! -->
+      <AddStory/>
     </div>
   );
 }
