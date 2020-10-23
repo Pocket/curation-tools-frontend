c6df823a6e5d6bfae2a3d005fc02549d06c8e03c
qmya <qandeelgul90@gmail.com>
2020-10-22 20:36:26 -0400

    added material design and use it also delete the css file

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <!-- your component goes here to test the rendering! --> */}
-      <h1>hello world </h1>
       <Button/>
     </div>
   );
--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,14 +1,90 @@
 import React from 'react';
-import './Button.css'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
 
-function ButtonCommponent() {
+const useStyles = makeStyles({
+  body: {
+    margin :0,
+    padding: "20px"
+  },
+  column:{
+    display: "flex",
+    flexDirection: "column",
+    marginTop: "10px"
+    
+
+  },
+  nav: {
+    display: "flex",
+    justifyContent: "flex-end"
+    
+    
+  },
+  threebuttons: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "30px"
+  },
+  positive : {
+    backgroundColor:"  #17bbbc",
+    border: "1px solid  #17bbbc",
+    padding: "10px",
+    borderRadius: "4px",
+    color: "white",
+    "&:hover":{
+      backgroundColor: "white",
+      color: "#17bbbc"
+    }
+  },
+  negative : {
+    backgroundColor:" #b74a0e",
+    border: "1px solid #b74a0e",
+    marginRight: "20px",
+    padding: "10px",
+    borderRadius: "4px",
+    color: "white",
+    "&:hover":{
+      backgroundColor: "white",
+      color: "#b74a0e"
+    }
+    
+  }, 
+  neutral : {
+    backgroundColor:" #686767",
+    border: "1px solid #686767",
+    marginRight: "20px",
+    padding: "10px",
+    borderRadius: "4px",
+    color: "white",
+    "&:hover":{
+      backgroundColor: "white",
+      color: "#686767"
+    }
+  },
+  hollow: {
+    backgroundColor: "white",
+    color: "#17bbbc",
+    padding: "10px",
+    marginLeft: "20px",
+    border: "1px solid #17bbbc",
+  }
+}); 
+
+function ButtonComponent() {
+  const classes = useStyles();
     return (
-      <div className="buttons">
-        <button className="negative" type="submit" > Reject </button>
-        <button className="neutral" type="submit" > Snooze </button>
-        <button className="positive" type="submit" > Edit & Approve </button>
+<div className= {classes.column}>
+         <div className = {classes.nav}>
+           <Button className={classes.positive} > Add Story </Button>
+          <Button className={classes.hollow} > Log Out </Button>
+        </div>
+        <div className = {classes.threebuttons}>
+        <Button className={classes.negative} > Reject </Button>
+        <Button className={classes.neutral} > Snooze </Button>
+        <Button className={classes.positive} > Edit & Approve </Button>
+      </div>
       </div>
     );
   }
   
-  export default ButtonCommponent;
+  export default ButtonComponent;
