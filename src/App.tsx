94d09823b87fd798f0b86f7ec7bc549452a01b18
Nina Pypchenko <22447785+nina-py@users.noreply.github.com>
2020-10-24 13:00:20 +1100

    Added a CardText component
    This component renders a block that is meant to go inside the Card
    component (#62) with the following properties:
    
    - Title
    - URL
    - Short description
    - Publisher
    - Author
    
    A couple of unit tests have been added to test the CardText component,
    and the CRA-provided default test for App marked as skipped as it no
    longer passes and is not needed just yet.
    
    I have also changed the comment line in App.tsx from <!-- --> to {/* */}
    as the former produces a TypeScript error and doesn't let the test suite run.
    
    Resolves #64.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { CardText } from "./components/CardText/CardText";
+
 function App() {
   return (
     <div className="App">
-      <!-- your component goes here to test the rendering! -->
+      {/* your component goes here to test the rendering! */}
+      <CardText
+        publisher="Atlas Obscura"
+        author="Sarah Lascow"
+        url="https://www.atlasobscura.com/articles/colonial-india-british-hedge-salt-tax"
+        title="The British Once Built a 1,100-Mile Hedge Through the Middle of India"
+        shortDesc="This quixotic colonial barrier was meant to enforce taxes."
+      />
     </div>
   );
 }
