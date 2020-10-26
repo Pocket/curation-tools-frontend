a21d01d63c96521ca2f81e36479f80be878fe56c
Joaquin-M2 <joaquinmorenomolina@gmail.com>
2020-10-26 17:21:29 +0100

    Include Card component - Ready to merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import PocketCard from './components/Card/Card';
 
 function App() {
     return (
         <div className="App">
-            {/* <!-- your component goes here to test the rendering! --> */}
-            <PocketCard imagePath="/pocket-shield@2x.png" />
+            <!-- your component goes here to test the rendering! -->
         </div>
     );
 }
