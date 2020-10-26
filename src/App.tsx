ff2138410773a50d87993067e6f0e6fdc03c1475
Joaquin-M2 <joaquinmorenomolina@gmail.com>
2020-10-26 18:10:51 +0100

    Include Card component - Ready to merge (really!)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <!-- your component goes here to test the rendering! -->
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* <!-- your component goes here to test the rendering! --> */}
+    </div>
+  );
 }
 
 export default App;
