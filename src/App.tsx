5fe0af56a06d05cfe433d1dfbc22c7fa12be9329
qmya <qandeelgul90@gmail.com>
2020-11-05 00:10:31 -0500

    fixed the delete contribution of peers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Button } from './components/ButtonComponent';
+import logo from './logo.svg';
 import './App.css';
-import styled from "styled-components";
-
-const Container = styled.div`
-display:flex;
-`;
 
 function App() {
   return (
     <div className="App">
-      <Container>
-      <Button text=" Add Story " variant="contained" size="large"  buttonType="positive"/>
-      <Button text=" Log Out " variant="outlined" size="small"  buttonType="hollow"/>
-      </Container>
-      <Container>
-      <Button text=" Reject " variant="contained" size="small"  color="secondary"/>
-      <Button text=" Snooze " variant="contained" size="medium" buttonType="neutral"/>
-      <Button text=" Edit & Approve " variant="contained" size="large" buttonType="positive"/>
-      </Container>
+      <!-- your component goes here to test the rendering! -->
     </div>
   );
 }
