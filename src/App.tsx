b650277ce4799dba7404de22c635c49bb3553edb
qmya <qandeelgul90@gmail.com>
2020-11-18 20:19:04 -0500

    modified Modal component using material Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Modal}  from './components/Modal/Modal';
+import { Button } from './components/ButtonComponent/ButtonComponent';
+import loadingGif from './assets/loading.gif';
 
-function App() {
-  return (
-    <div className="App">
-      <!-- your component goes here to test the rendering! -->
-    </div>
-  );
-}
+function App(){
+  const [isModalOpen, setModalState] = React.useState(false);
+  const toggleModal = () => setModalState(!isModalOpen);
+  	  return (
+      <div className="App">	    
+       <Button
+        onClick={toggleModal} text="Parse" buttonType="positive" size="small"
+      />
+      <Modal
+        title='Processing results...'
+        isOpen={isModalOpen}
+        onClose={toggleModal}
+        image={loadingGif}
+      />
+    </div>	    
+  );	  
+}	
 
 export default App;
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Dialog } from "@material-ui/core";
+
+interface ModalProps {
+  title: string;
+  image: string;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      width: 350,
+      height:70,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      display: "flex",
+      justifyContent:"center",
+      alignItems: "center"
+    },  
+  }),
+);
+export const Modal: React.FC<ModalProps> = ({ title, isOpen, image, onClose}) => {
+    const classes = useStyles(); 
+    return (
+      <Dialog open={isOpen} onBackdropClick={onClose}>
+          <div className={classes.paper}>
+            <div id="simple-modal-title"> {title} </div>
+            <img src={image} alt="loading gif to show that data is loading"/>
+          </div>
+        </Dialog>
+  
+  )
+};
